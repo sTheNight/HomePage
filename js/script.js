72726e724f146c002399731442074e8c5832d36a
重铬酸钠 <sthenight@outlook.com>
2025-05-03 19:03:01 +0800

    禁止复制页面文字

--- js/script.js
+++ js/script.js
@@ -1,1 +1,4 @@
-/* 暂时啥也没有~ */
+document.oncontextmenu = () => { return false; }
+document.onselectstart = () => { return false; }
+document.oncopy = () => { return false; }
+document.oncut = () => { return false; }
