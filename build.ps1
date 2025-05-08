404bd6218f88016906b3997edb92c15cdf1880aa
重铬酸钠 <sthenight@outlook.com>
2025-05-08 20:14:44 +0800

    优化样式效果，增加 ps1 脚本

---
+++ build.ps1
@@ -0,0 +1,2 @@
+
+lessc less/styles.less css/styles.css && lessc less/apapte.less css/apapte.css
--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
+# 此脚本用于 Cloudflare Pages 快速构建页面
 npm install -g less && lessc less/styles.less css/styles.css && lessc less/apapte.less css/apapte.css
