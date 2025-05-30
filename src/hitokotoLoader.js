2d717f275ae71ae7bcbcc734001847531636074a
重铬酸钠 <sthenight@outlook.com>
2025-05-31 07:42:02 +0800

    本次为提交暂存 1. 对接 Hitokoto API 2. 优化 left div 样式

---
+++ src/hitokotoLoader.js
@@ -0,0 +1,5 @@
+export async function fetchHitokoto() {
+  const response = await fetch('https://v1.hitokoto.cn?c=d&c=k')
+  const { hitokoto: hitokotoText } = await response.json()
+  document.querySelector("#Hitokoto").innerHTML = hitokotoText;
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 import './styles.less'
+import './left.less'
+import './right.less'
+import { fetchHitokoto } from './hitokotoLoader'
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchHitokoto()
+})
