566820c64d0e920219ad55a2dac49311da51f87d
重铬酸钠 <sthenight@outlook.com>
2025-05-31 09:22:47 +0800

    Test

--- src/hitokotoLoader.js
+++
@@ -1,5 +0,0 @@
-export async function fetchHitokoto() {
-  const response = await fetch('https://v1.hitokoto.cn?c=d&c=k')
-  const { hitokoto: hitokotoText } = await response.json()
-  document.querySelector("#Hitokoto").innerHTML = hitokotoText;
-}
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import './styles.less'
 import './left.less'
 import './right.less'
-import { fetchHitokoto } from './hitokotoLoader'
 
-document.addEventListener('DOMContentLoaded', () => {
-    fetchHitokoto()
+async function fetchHitokoto() {
+  const response = await fetch('https://v1.hitokoto.cn?c=d&c=k')
+  const { hitokoto: hitokotoText } = await response.json()
+  document.querySelector("#Hitokoto").innerHTML = hitokotoText;
+}
+
+document.addEventListener('DOMContentLoaded',()=> {
+    fetchHitokoto();
 })
