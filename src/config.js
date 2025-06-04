aaf6ee9f077d8bae84dc038cdd9d5d57f2504456
重铬酸钠 <sthenight@outlook.com>
2025-06-04 22:37:35 +0800

    将自定义功能移动至 config.js 中

---
+++ src/config.js
@@ -0,0 +1,15 @@
+export default {
+    avatar: 'https://avatars.githubusercontent.com/u/85998341?v=4',
+    author: '重铬酸钠',
+    hitokoto: '#HitokotoEnable#',
+    // name 无实际意义，仅用于标识此行用于表示什么
+    // 按钮图标通过 icon 与 customIcon 共同决定
+    // customIcon 为 false 则使用 fontawesome 图标库，此时 icon 处需要填入 fontawesome 图标库的类名
+    // 若 customIcon 为 true 你可以在 icon 处填入任意的 HTML 标签，当然此时的样式需要你自己去处理
+    socialLinks: [
+        { name: 'Telegram', icon: 'fa-brands fa-telegram', link: 'https://t.me/SodiumDichromate', customIcon: false },
+        { name: 'Email', icon: 'fa-solid fa-envelope', link: 'mailto:sthenight@outlook.com', customIcon: false },
+        { name: 'Github', icon: 'fa-brands fa-github', link: 'https://github.com/sTheNight', customIcon: false },
+        { name: 'Bilibili', icon: 'fa-solid fa-envelope', link: 'https://space.bilibili.com/255841070', customIcon: false }
+    ]
+}
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import './styles.less'
 import './left.less'
 import './right.less'
+import config from './config.js'
 
 document.addEventListener('DOMContentLoaded', () => {
+  fillBasicInfo();
+  generateSocialButton()
   calcNavGroupWidth()
-  fetchHitokoto()
   bindNavcardEvent()
   document.querySelector('.avatar').addEventListener('click', () => {
     location.href = 'https://www.github.com/sTheNight'
@@ -21,7 +23,7 @@ function calcNavGroupWidth() {
 async function fetchHitokoto() {
   const response = await fetch('https://v1.hitokoto.cn?c=d&c=k')
   const { hitokoto: hitokotoText } = await response.json()
-  document.querySelector("#Hitokoto").innerHTML = hitokotoText
+  return hitokotoText
 }
 
 function bindNavcardEvent() {
@@ -41,4 +43,28 @@ function bindNavcardEvent() {
       }
     })
   })
+}
+//<a class="social-button" href="https://t.me/SodiumDichromate" target="_blank"><i
+//  class="fa-brands fa-telegram"></i></a>
+function generateSocialButton() {
+  const { socialLinks } = config
+  const contactDiv = document.querySelector('.contact')
+  socialLinks.forEach(item => {
+    const btn = document.createElement('a')
+    btn.className = 'social-button'
+    btn.href = item.link
+    btn.innerHTML = item.customIcon ? item.icon : `<i class="${item.icon}"></i>`
+    contactDiv.appendChild(btn)
+  })
+}
+
+async function fillBasicInfo() {
+  const { author, hitokoto,avatar } = config
+  const authorNameEle = document.querySelector('#name')
+  const hitokotoEle = document.querySelector('#Hitokoto')
+  const avatarEle = document.querySelector('.avatar')
+
+  authorNameEle.innerText = author
+  hitokotoEle.innerText = hitokoto == '#HitokotoEnable#' ? await fetchHitokoto() : hitokoto
+  avatarEle.style.backgroundImage = `url("${avatar}")`;
 }
