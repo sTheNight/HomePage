76bd35076bbd07329d53ddd64cd39f0d3ad2d48b
重铬酸钠 <sthenight@outlook.com>
2025-06-16 17:39:54 +0800

    类型约束

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import '../less/styles.less'
 import '../less/left.less'
 import '../less/right.less'
 import { debounce } from './utils.mts'
-import config from './config.mts'
+import { config } from './config.mts'
 
 document.addEventListener('DOMContentLoaded', () => {
     fillBasicInfo();
@@ -13,18 +13,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
 window.addEventListener('resize', debounce(calcNavGroupWidth, 500))
 
-function calcNavGroupWidth() {
-    const cardWidth = document.querySelector<HTMLDivElement>('.nav-card-div')!.offsetWidth
-    document.querySelector<HTMLDivElement>(".nav-group")!.style.maxWidth = `${(cardWidth * 3)}px`
+function calcNavGroupWidth(): void {
+    const cardWidth = document.querySelector<HTMLDivElement>('.nav-card-div')?.offsetWidth
+    if (cardWidth) document.querySelector<HTMLDivElement>(".nav-group")!.style.maxWidth = `${(cardWidth * 3)}px`
 }
 
-async function fetchHitokoto() {
+async function fetchHitokoto(): Promise<string> {
     const response = await fetch('https://v1.hitokoto.cn?c=d&c=k')
     const { hitokoto: hitokotoText } = await response.json()
     return hitokotoText
 }
 
-function generateSocialButton() {
+function generateSocialButton(): void {
     const { socialLinks } = config
     const contactDiv = document.querySelector<HTMLDivElement>('.contact')
     socialLinks.forEach(item => {
@@ -32,11 +32,11 @@ function generateSocialButton() {
         btn.className = 'social-button'
         btn.href = item.link
         btn.innerHTML = item.customIcon ? item.icon : `<i class="${item.icon}"></i>`
-        contactDiv!.appendChild<HTMLAnchorElement>(btn)
+        contactDiv?.appendChild<HTMLAnchorElement>(btn)
     })
 }
 
-function generateNavCard() {
+function generateNavCard(): void {
     const { navItems } = config
     const navGroupEle = document.querySelector<HTMLDivElement>('.nav-group')
     navItems.forEach(item => {
@@ -44,31 +44,38 @@ function generateNavCard() {
         newNavCard.classList.add('nav-card-div')
         newNavCard.innerHTML = `<i class="fa-solid fa-link"></i>${item.name}`
         newNavCard.addEventListener('click', () => {
-            location.href = item.link
+            location.assign(item.link)
         })
-        navGroupEle!.appendChild<HTMLDivElement>(newNavCard)
+        navGroupEle?.appendChild<HTMLDivElement>(newNavCard)
     })
 }
 
-async function fillBasicInfo() {
+async function fillBasicInfo(): Promise<void> {
     const { author, hitokoto, avatar, favicon, title } = config
-    const authorNameEle = document.querySelector<HTMLPreElement>('#name')
-    const hitokotoEle = document.querySelector<HTMLPreElement>('#Hitokoto')
-    const avatarEle = document.querySelector<HTMLDivElement>('.avatar')
 
-    authorNameEle!.innerText = author
-    hitokotoEle!.innerText = hitokoto == '#HitokotoEnable#' ? await fetchHitokoto() : hitokoto
-    avatarEle!.style.backgroundImage = `url("${avatar}")`;
-    document.querySelector('.avatar')?.addEventListener('click', () => {
-        location.href = 'https://www.github.com/sTheNight'
-    })
+    const nameEl = document.querySelector<HTMLPreElement>('#name')
+    if (nameEl) nameEl.innerText = author
 
-    const faviconEle: HTMLLinkElement = document.createElement('link')
-    faviconEle.referrerPolicy = 'icon'
-    faviconEle.href = favicon
-    document.head.appendChild<HTMLLinkElement>(faviconEle)
+    const hitokotoEl = document.querySelector<HTMLPreElement>('#Hitokoto')
+    if (hitokotoEl)
+        hitokotoEl.innerText = hitokoto === '#HitokotoEnable#'
+            ? await fetchHitokoto()
+            : hitokoto
 
-    const titleEle: HTMLTitleElement = document.createElement('title')
-    titleEle.innerText = title
-    document.head.appendChild<HTMLTitleElement>(titleEle)
+    const avatarEl = document.querySelector<HTMLDivElement>('.avatar')
+    if (avatarEl) {
+        avatarEl.style.backgroundImage = `url("${avatar}")`
+        avatarEl.addEventListener('click', () => {
+            location.assign('https://www.github.com/sTheNight')
+        })
+    }
+
+    const faviconEl = document.createElement('link')
+    faviconEl.rel = 'icon'
+    faviconEl.href = favicon
+    document.head.appendChild(faviconEl)
+
+    const titleEl = document.createElement('title')
+    titleEl.innerText = title
+    document.head.appendChild(titleEl)
 }
