fa3ad95e0a0e8652b7908477ec99ad89f4d804cf
重铬酸钠 <sthenight@outlook.com>
2025-08-21 14:11:47 +0800

    更正标题

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { config } from './config'
 
 createApp(App).mount('#app')
+
+
+const titleEl = document.createElement('title')
+titleEl.innerText = config.title
+document.head.appendChild(titleEl)
